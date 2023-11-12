8f272991145f9090d99e80f14cd430d4884dd164
Brendo <brendow_prime@hotmail.com>
2023-11-11 20:56:35 -0400

    Feat/ Update tables PrismaSchema

---
+++ prisma/migrations/20231112005437_add_tables_participant_game_bet/migration.sql
@@ -0,0 +1,49 @@
+/*
+  Warnings:
+
+  - You are about to drop the `news` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "news";
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "homeTeamName" TEXT NOT NULL,
+    "awayTeamName" TEXT NOT NULL,
+    "homeTeamScore" INTEGER NOT NULL,
+    "awayTeamScore" INTEGER NOT NULL,
+    "isFinished" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bet" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "homeTeamScore" INTEGER NOT NULL,
+    "awayTeamScore" INTEGER NOT NULL,
+    "amountBet" INTEGER NOT NULL,
+    "gameId" INTEGER NOT NULL,
+    "participantId" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "amountWon" INTEGER,
+
+    CONSTRAINT "Bet_pkey" PRIMARY KEY ("id")
+);
