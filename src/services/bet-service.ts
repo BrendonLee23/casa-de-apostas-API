ee96743660856af39141f0a1a98fe165b8bc20b8
Brendo <brendow_prime@hotmail.com>
2023-11-20 02:13:35 -0400

    Fix/ postBet updates

--- src/services/bet-service.ts
+++ src/services/bet-service.ts
@@ -13,7 +13,7 @@ async function postBet(betBody: CreateBet): Promise<Bet> {
   const amountBet = betBody.amountBet;
   const participantBalance = participant.balance;
   // Verificar se o saldo do participante é suficiente ou se não é negativo/0
-  if (participantBalance >= 0) throw badRequestError('Balance cannot be negative or 0');
+  if (amountBet <= 0) throw badRequestError('Balance cannot be negative or 0');
   if (participantBalance < amountBet) throw paymentRequiredError('Balance must be greater than or equal to amount');
   // Deduzir imediatamente o valor da aposta do saldo do participante
   await participantRepository.updateParticipantBalanceById(id, participantBalance, amountBet);
