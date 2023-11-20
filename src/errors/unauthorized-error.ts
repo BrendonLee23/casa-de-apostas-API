8a0154cc8aaa31dbbadc281e42d05b57039df65e
Brendo <brendow_prime@hotmail.com>
2023-11-20 02:28:35 -0400

    Fix/ add badRequest to finished games on postBet

--- src/errors/unauthorized-error.ts
+++ src/errors/unauthorized-error.ts
@@ -1,10 +1,10 @@
 import httpStatus from 'http-status';
 import { ApplicationError } from '../protocols';
 
-export function unauthorizedError(): ApplicationError {
+export function unauthorizedError(info: string): ApplicationError {
   return {
     name: 'unauthorizedError',
-    message: 'Not authorized to access',
+    message: `Not authorized to proceed: ${info}`,
     status: httpStatus.UNAUTHORIZED,
   };
 }
--- src/services/bet-service.ts
+++ src/services/bet-service.ts
@@ -4,6 +4,7 @@ import { paymentRequiredError } from '../errors/payment-required-error';
 import { CreateBet } from '../protocols';
 import { betRepository } from '../repositories/bet-repository';
 import { participantRepository } from '../repositories/participant-repository';
+import { gameRepository } from './../repositories/game-repository';
 import { badRequestError } from 'src/errors/bad-request-error';
 
 async function postBet(betBody: CreateBet): Promise<Bet> {
@@ -12,12 +13,12 @@ async function postBet(betBody: CreateBet): Promise<Bet> {
   if (!participant) throw notFoundError('Participant not found');
   const amountBet = betBody.amountBet;
   const participantBalance = participant.balance;
-  // Verificar se o saldo do participante é suficiente ou se não é negativo/0
   if (amountBet <= 0) throw badRequestError('Balance cannot be negative or 0');
   if (participantBalance < amountBet) throw paymentRequiredError('Balance must be greater than or equal to amount');
-  // Deduzir imediatamente o valor da aposta do saldo do participante
+  const gameId = betBody.gameId;
+  const gameFinished = await gameRepository.findGameById(gameId);
+  if (gameFinished) throw badRequestError('This game has already been finished');
   await participantRepository.updateParticipantBalanceById(id, participantBalance, amountBet);
-  // Criar a aposta
   const result = await betRepository.createBet(betBody);
   return result;
 }
